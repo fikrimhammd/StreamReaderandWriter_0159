98331b4513407484e05d0f5ff971c5114b5352d3
fikrimhammd <fikri.kdi60@gmail.com>
2024-06-04 10:18:03 +0700

    int main ioFile

--- ioFile.cpp
+++ ioFile.cpp
@@ -3,6 +3,16 @@
 #include <string>
 using namespace std;
 
+int main() {
 
+	//untuk menyimpan data berbentuk string
+	string baris;
+
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
 
