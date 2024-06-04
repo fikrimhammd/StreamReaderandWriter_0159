4478c82a5e74c22822554a0f3d2c8b48c798524e
fikrimhammd <fikri.kdi60@gmail.com>
2024-06-04 10:52:46 +0700

    menampilkan if dalam iofilepath

--- IoFileWithPath/IoFileWithPath.cpp
+++ IoFileWithPath/IoFileWithPath.cpp
@@ -27,4 +27,28 @@ int main() {
 		//menulis dan memasukkan nilai dari 'baris' ke dalam file
 		outfile << baris << endl;
 	}
+	//selesai dalam menulis sekarang tutup file nya
+	outfile.close();
+
+
+	//membuka file dalam mode membaca
+	ifstream infile;
+
+	//menunjuk ke sebuah file
+	infile.open(NamaFile + ".txt", ios::in);
+
+	cout << endl << ">= Membuka dan membaca file" << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
 	
+}
